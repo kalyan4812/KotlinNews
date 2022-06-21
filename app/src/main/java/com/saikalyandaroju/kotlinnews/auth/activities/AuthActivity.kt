1b3638fa9985fba19ef941d250e9ffe36110339d
kalyan4812 <darojusaikalyan.civ18@itbhu.ac.in>
2022-06-21 23:19:57 +0530

    added firebase dependency

---
+++ app/src/main/java/com/saikalyandaroju/kotlinnews/auth/activities/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.saikalyandaroju.kotlinnews.auth.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saikalyandaroju.kotlinnews.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+}
